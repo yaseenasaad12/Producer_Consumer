0073731d5b521a8befb3716110ee2db1c91ec0f9
Yaseen_Asaad <yaseenasaad12@gmail.com>
2024-12-23 05:26:43 +0200

    update UI

--- front-end/src/Shape.jsx
+++ front-end/src/Shape.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Shape = ({ type }) => {
   return (
     <div className={`shape ${type}`}>
-      {type === 'machine' && <div>Machine</div>}
-      {type === 'queue' && <div>Queue</div>}
-      {type === 'link' && <div>Link</div>}
+      
     </div>
   );
 };
